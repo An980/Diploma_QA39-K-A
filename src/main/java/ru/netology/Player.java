18d34f914d1618a901c831622dd3af82ce630eac
Konstantin Velly <konstantinvelly@gmail.com>
2022-08-23 13:28:40 +0300

    Issues 1-3

--- src/main/java/ru/netology/Player.java
+++ src/main/java/ru/netology/Player.java
@@ -9,7 +9,7 @@ public class Player {
     /** информация о том, в какую игру сколько часов было сыграно
     ключ - игра
     значение - суммарное количество часов игры в эту игру */
-    private Map<Game, Integer> playedTime = new HashMap<>();
+    private HashMap<Game, Integer> playedTime = new HashMap<>();
 
     public Player(String name) {
         this.name = name;
@@ -44,13 +44,12 @@ public class Player {
      суммирует время, проигранное во все игры этого жанра этим игроком */
     public int sumGenre(String genre) {
         int sum = 0;
-        for (Game game : playedTime.keySet()) {
+        for (Game game : playedTime.keySet())
             if (game.getGenre().equals(genre)) {
                 sum += playedTime.get(game);
             } else {
                 sum = 0;
             }
-        }
         return sum;
     }
 
